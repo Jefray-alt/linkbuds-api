23528bcd88e660689ce386e75fbea993e7bf7a6a
Jeffrey France <jbfrance1626@gmail.com>
2024-03-12 17:33:52 +0800

    create jwt util

---
+++ src/utils/auth.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+interface JWTPayload {
+  userId: string;
+  username: string;
+  email: string;
+}
+
+export const generateJWT = (payload: JWTPayload, expiresIn = '5m'): string => {
+  const secretKey = process.env.JWT_SECRET_KEY;
+  if (secretKey === undefined || secretKey === null) {
+    throw new Error('JWT secret key not provided');
+  }
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn });
+};
