cc2e01a9304aa78d8d29c93a60726265269c23fd
Jeffrey France <jbfrance1626@gmail.com>
2024-03-14 15:22:12 +0800

    implement cookie parser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AppDataSource } from './data-source';
 import errorHandler from './middleware/error.middleware';
 import router from './routes';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
 import 'reflect-metadata';
@@ -13,6 +14,7 @@ const port = process.env.PORT ?? 3000;
 void (async function mainModule() {
   try {
     await AppDataSource.initialize();
+    app.use(cookieParser());
     app.use(router);
     app.use(errorHandler);
 
