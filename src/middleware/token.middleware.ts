d03e29638cc45ea657289c15415b2cd474f19c72
Jeffrey France <jbfrance1626@gmail.com>
2024-03-15 13:01:28 +0800

    add jwt middleware to specific routes

--- src/middleware/token.middleware.ts
+++ src/middleware/token.middleware.ts
@@ -1,3 +1,4 @@
+import { nonSecurePaths } from '../routes/paths';
 import { type UserJwtPayload } from '../utils/auth';
 import { UnauthorizedError } from '../utils/errors';
 import { type NextFunction, type Request, type Response } from 'express';
@@ -9,6 +10,10 @@ export const tokenDecode = (
   next: NextFunction
 ): void => {
   try {
+    if (nonSecurePaths.includes(req.path)) {
+      next();
+      return;
+    }
     const authHeader = req.headers.authorization;
     const token = authHeader?.split(' ')[1];
     if (token === null || token === undefined) {
---
+++ src/routes/paths.ts
@@ -0,0 +1,1 @@
+export const nonSecurePaths = ['/', '/auth/login', '/auth/register'];
