e02f6e7dd6307c98b6940178fb90705f8a759bcf
Jeffrey France <jbfrance1626@gmail.com>
2024-03-15 13:00:48 +0800

    fix types

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,7 +3,7 @@ import { type UserJwtPayload } from '../utils/auth';
 declare global {
   namespace Express {
     export interface Request {
-      user?: UserJwtPayload;
+      user: UserJwtPayload;
     }
   }
 }
